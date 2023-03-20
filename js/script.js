02290c3ebf27d5835094bf3fe8cf9f673e64e11a
michael-borck <91189447+michael-borck@users.noreply.github.com>
2023-03-20 18:02:29 +0800

    fixed class update

--- js/script.js
+++ js/script.js
@@ -15,7 +15,18 @@ function onAddTaskClicked(event) {
 
 function onTodoListContainerClicked(event){
    var targetElement = event.toElement;
-
+   
+   while (!targetElement.classList.contains("task")){
+      targetElement = targetElement.parentElement;
+   } 
+   
+   var checkbox = targetElement.querySelector(".checkbox");
+   if (checkbox.checked){
+      targetElement.classList.add("completed")
+   } else {
+      targetElement.classList.remove("completed")
+   }
+}
 // Step 3 link to event handler
 addTaskButton.addEventListener('click', onAddTaskClicked);
 todoListContainer.addEventListener('click', onTodoListContainerClicked);
