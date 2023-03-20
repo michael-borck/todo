08a6a5d72041ca182dafd0e6141d5bdbf3e82826
michael-borck <91189447+michael-borck@users.noreply.github.com>
2023-03-20 17:49:54 +0800

    add class update to completed

--- js/script.js
+++ js/script.js
@@ -13,6 +13,19 @@ function onAddTaskClicked(event) {
     todoListContainer.insertAdjacentHTML('afterbegin', taskHTML);
 }
 
+function onTodoListContainerClicked(event){
+   while (!targetElement.classList.contains("task")){
+    targetElement = targetElement.parentElement;
+   } 
+   var checkbox = targetElement.querySelector(".checkbox");
+   if (checkbox.checked){
+    targetElement.classList.add("completed")
+   } else {
+    targetElement.classList.remove("completed")
+   }
+}
+
 
 // Step 3 link to event handler
 addTaskButton.addEventListener('click', onAddTaskClicked);
+todoListContainer.addEventListener('click', onTodoListContainerClicked);
