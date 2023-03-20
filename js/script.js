a6c88ef8dd38d2c097fe652f6866d1a73b306565
michael-borck <91189447+michael-borck@users.noreply.github.com>
2023-03-20 18:01:09 +0800

    fixed sync

--- js/script.js
+++ js/script.js
@@ -13,46 +13,9 @@ function onAddTaskClicked(event) {
     todoListContainer.insertAdjacentHTML('afterbegin', taskHTML);
 }
 
-<<<<<<< HEAD
 function onTodoListContainerClicked(event){
    var targetElement = event.toElement;
-=======
-function onTodoListContainerClicked(event){
-   while (!targetElement.classList.contains("task")){
-    targetElement = targetElement.parentElement;
-   } 
-   var checkbox = targetElement.querySelector(".checkbox");
-   if (checkbox.checked){
-    targetElement.classList.add("completed")
-   } else {
-    targetElement.classList.remove("completed")
-   }
-}
->>>>>>> 08a6a5d72041ca182dafd0e6141d5bdbf3e82826
 
-<<<<<<< HEAD
-   while (!targetElement.classList.contains("task")){
-    targetElement = targetElement.parentElement;
-   } 
-
-   var checkbox = targetElement.querySelector(".checkbox");
-   if (checkbox.checked){
-    targetElement.classList.add("completed")
-   } else {
-    targetElement.classList.remove("completed")
-   }
-   
-}
-
-
-=======
-
->>>>>>> 08a6a5d72041ca182dafd0e6141d5bdbf3e82826
 // Step 3 link to event handler
 addTaskButton.addEventListener('click', onAddTaskClicked);
-<<<<<<< HEAD
 todoListContainer.addEventListener('click', onTodoListContainerClicked);
-=======
-todoListContainer.addEventListener('click', onTodoListContainerClicked);
-
->>>>>>> 08a6a5d72041ca182dafd0e6141d5bdbf3e82826
